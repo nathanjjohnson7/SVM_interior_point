76701dbb0f616308a2548660f1875db72a067601
nathanjjohnson7 <77037654+nathanjjohnson7@users.noreply.github.com>
2025-06-05 14:11:07 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch as T
 
 def accuracy(preds, labels):
+    #labels are 1 for positive and -1 for negative
     return T.sum(((preds >= 0)*2)-1 == labels.squeeze())/labels.squeeze().shape[0]
